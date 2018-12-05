2208c75e5707547bcd4d77bfd5f8f9849df1aef2
Manyanda Chitimbo <manyanda.chitimbo@gmail.com>
2018-12-05 22:08:15 +0100

    Initial commit

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+declare const TsMuchi: (...arg) => ClassDecorator;
+declare const Only: (...arg) => ClassDecorator;
+declare const Context: (...arg) => MethodDecorator;
+declare const After: MethodDecorator;
+declare const Before: MethodDecorator;
+declare const Test: (...arg) => MethodDecorator;
+declare const Mock: any;
